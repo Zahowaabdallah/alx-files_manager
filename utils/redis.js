23dc0e8696336167e3487437e6441d1ab52e2c05
Zahowaabdallah <zahwaabdallah1@gmail.com@example.com>
2024-09-06 09:08:38 -0700

    alx-files_manager

---
+++ utils/redis.js
@@ -0,0 +1,46 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err);
+    });
+    this.client.connect();
+  }
+  
+  isAlive() {
+    return this.client.isReady;
+  }
+  
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.error('Error getting value from Redis:', err);
+      return null;
+    }
+  }
+  
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, {
+        EX: duration
+      });
+    } catch (err) {
+      console.error('Error setting value in Redis:', err);
+    }
+  }
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error('Error deleting value from Redis:', err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
